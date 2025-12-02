ee47976b0716297dcc6df982c9607f689a7f1921
Дмітрій <mrfoxxx365@gmail.com>
2025-12-02 14:23:38 +0200

    Physical Replication

--- db/replications/publisher/010_create_publication.sql
+++
@@ -1,2 +0,0 @@
--- Публікуємо дані з обох таблиць
-CREATE PUBLICATION my_travel_publication FOR ALL TABLES;
--- db/replications/publisher/020_pg_hba.sh
+++ db/replications/publisher/020_pg_hba.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
-# Дозволити subscriber підключатися по паролю до всіх баз під користувачем rep_user
-echo "host all rep_user 0.0.0.0/0 md5" >> "${PGDATA}/pg_hba.conf"
 
+# Дозволяємо підключення для реплікації
+echo "host replication rep_user 0.0.0.0/0 md5" >> "${PGDATA}/pg_hba.conf"
+
+# Перезавантажуємо конфігурацію
 pg_ctl reload
