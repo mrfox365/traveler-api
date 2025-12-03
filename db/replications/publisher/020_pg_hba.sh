91d1e9cd8acf854d17b7006e6d6049813073bee7
Дмітрій <mrfoxxx365@gmail.com>
2025-12-03 17:26:51 +0200

    pgcat

--- db/replications/publisher/020_pg_hba.sh
+++ db/replications/publisher/020_pg_hba.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Налаштовуємо PostgreSQL на використання SCRAM-SHA-256 для шифрування паролів
-echo "password_encryption = scram-sha-256" >> "${PGDATA}/postgresql.conf"
-
 # Дозволяємо підключення для реплікації (використовуємо змінну)
-echo "host replication $REP_USER 0.0.0.0/0 md5" >> "${PGDATA}/pg_hba.conf"
+echo "host replication all 0.0.0.0/0 trust" >> "${PGDATA}/pg_hba.conf"
+echo "host all all 0.0.0.0/0 trust" >> "${PGDATA}/pg_hba.conf"
 
 # Перезавантажуємо конфігурацію
 pg_ctl reload
