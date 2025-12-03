becb7147d728190ecf903c8d0ae649ff8742df49
Дмітрій <mrfoxxx365@gmail.com>
2025-12-03 12:17:54 +0200

    Security 2

--- db/replications/publisher/020_pg_hba.sh
+++ db/replications/publisher/020_pg_hba.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Налаштовуємо PostgreSQL на використання SCRAM-SHA-256 для шифрування паролів
+echo "password_encryption = scram-sha-256" >> "${PGDATA}/postgresql.conf"
+
 # Дозволяємо підключення для реплікації (використовуємо змінну)
 echo "host replication $REP_USER 0.0.0.0/0 md5" >> "${PGDATA}/pg_hba.conf"
 
